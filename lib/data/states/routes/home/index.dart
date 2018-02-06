f2134132d17dea73d68ec9300a956a5fcb4e0abf
Thomas Chen <foxnewsnetwork@gmail.com>
2018-02-05 19:57:50 -0800

    added home index chip row

--- lib/data/states/routes/home/index.dart
+++ lib/data/states/routes/home/index.dart
@@ -3,11 +3,16 @@ part of data;
 @immutable
 class HomeIndexRouteState {
   final String title;
-  final HamburgerModel hamburger; 
+  final HamburgerModel hamburger;
+  final List<TagModel> tags;
 
   const HomeIndexRouteState({
     this.title: 'Rental Market',
-    this.hamburger: HamburgerModel.initial
+    this.hamburger: HamburgerModel.initial,
+    this.tags: const <TagModel>[
+      const TagModel(displayName: 'Top Categories'),
+      const TagModel(displayName: 'Nearby')
+    ]
   });
 
   static const initial = const HomeIndexRouteState();
